cf68a7f944a127ab88485a62886bc01f8c3da775
mohaiminul375 <mohaiminul375@gmail.com>
2025-06-29 22:43:34 +0600

    auth is working

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ import express from "express";
 import cors from "cors";
 const app = express();
 // middleware
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true, 
+}));
 app.use(express.json());
 
 import userRouter from './routers/user.router.js';
--- routers/user.router.js
+++ routers/user.router.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { User } from "../models/user.model.js";
+import authenticateUser from "../middleware/verifyToken.js";
 import bcrypt from 'bcrypt';
 const router = Router();
 import jwt from "jsonwebtoken";
@@ -33,4 +34,61 @@ router.post('/register', async (req, res) => {
         res.status(500).json({ message: `failed to signup: ${error}` });
     }
 })
+
+// login for verifier
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(400).json({ message: 'All fields are required' });
+    }
+    try {
+        // Find user by email or phone_number
+        const user = await User.findOne({
+            email
+        });
+        if (!user) {
+            return res.status(400).json({ message: 'User not found' });
+        }
+        if (user?.status === 'Blocked') {
+            return res.status(403).json({ message: 'Your account is Blocked. Please contact support.' });
+        }
+        // Compare password with the stored hash
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+        // generate token
+        const token = jwt.sign(
+            {
+                id: user._id,
+                name: user.name,
+                email: user.email,
+            },
+            process.env.JWT_SECRET,
+            { expiresIn: '12h' }
+        );
+        res.json({ success: true, token, message: 'Login successful' });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal server error' });
+    }
+})
+
+// get user data after login
+router.get('/user-data', authenticateUser, async (req, res) => {
+
+    try {
+        // Find the user by email (from the decoded JWT)
+        const user = await User.findOne({ email: req.user.email });
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        // Remove password from the user object
+        const { password, ...userWithoutPass } = user.toObject();
+        console.log(userWithoutPass)
+        res.status(200).json({ user: userWithoutPass });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error fetching user data" });
+    }
+});
 export default router;
