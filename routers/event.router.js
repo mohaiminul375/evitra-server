7fd4435076d38998caef36877ad1aeeefdefe69b
mohaiminul375 <mohaiminul375@gmail.com>
2025-06-30 16:34:34 +0600

    filter all events

--- routers/event.router.js
+++ routers/event.router.js
@@ -1,17 +1,59 @@
 import { Router } from "express";
 const router = Router();
 import { Event } from "../models/event.model.js";
-
+import moment from "moment";
 
 // get all events
+
+
 router.get('/all-events', async (req, res) => {
     try {
-        const result = await Event.find().sort({ event_Date: -1 }); //-1 most recent events appear first
-        res.status(200).json(result)
+        const { search, sort, todayDate } = req.query;
+        const searchRegex = search ? new RegExp(search, "i") : null;
+
+        let query = {};
+        // Search filter
+        if (searchRegex) {
+            query.event_title = searchRegex;
+        }
+        // today date
+        if (todayDate) {
+            query.event_Date = {
+                $gte: moment().startOf("day").toISOString(),
+                $lte: moment().endOf("day").toISOString(),
+            }
+        }
+        // sort by date range
+        if (sort) {
+            if (sort === "current_week") {
+                query.event_Date = {
+                    $gte: moment().startOf("week").toISOString(), //gte:greater than or equal
+                    $lte: moment().endOf("week").toISOString(), //lte: less than or equal
+                };
+            } else if (sort === "last_week") {
+                query.event_Date = {
+                    $gte: moment().subtract(1, "weeks").startOf("week").toISOString(),
+                    $lte: moment().subtract(1, "weeks").endOf("week").toISOString(),
+                };
+            } else if (sort === "current_month") {
+                query.event_Date = {
+                    $gte: moment().startOf("month").toISOString(),
+                    $lte: moment().endOf("month").toISOString(),
+                };
+            } else if (sort === "last_month") {
+                query.event_Date = {
+                    $gte: moment().subtract(1, "months").startOf("month").toISOString(),
+                    $lte: moment().subtract(1, "months").endOf("month").toISOString(),
+                };
+            }
+        }
+        const result = await Event.find(query).sort({ event_Date: -1 });
+        res.status(200).json(result);
     } catch (error) {
-        res.status(500).json({ message: `failed to get tasks: ${error}` });
+        res.status(500).json({ message: `Failed to get events: ${error.message}` });
     }
-})
+});
+
 // get event by email
 router.get('/all-events/:email', async (req, res) => {
     try {
